59e9012eb3396c670e14d2e49d6f667fe581eb85
unknown <arbi.nmaki919@gmail.com>
2021-08-06 21:31:51 +0700

    Routes commit

--- index.js
+++ index.js
@@ -11,6 +11,19 @@ mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
   console.log("Connected to DATABASE");
 });
 
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+    res.send("welcome to homepage");
+});
+
+app.get("/users", (req, res) => {
+    res.send("welcome to user page");
+})
+
 app.listen(3000, () => {
   console.log("Backend server is running");
 });

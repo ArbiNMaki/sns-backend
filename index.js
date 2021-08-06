a99d9d20503aa50d4ab840982571724db9f633a2
unknown <arbi.nmaki919@gmail.com>
2021-08-06 21:39:47 +0700

    Test routes commit

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
+const userRoute = require("./routes/users");
+
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
@@ -16,13 +18,8 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-app.get("/", (req, res) => {
-    res.send("welcome to homepage");
-});
-
-app.get("/users", (req, res) => {
-    res.send("welcome to user page");
-})
+//route
+app.use("/api/users", userRoute);
 
 app.listen(3000, () => {
   console.log("Backend server is running");

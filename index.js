24e128af0ed75654e3a02cd1db19779c53172f03
unknown <arbi.nmaki919@gmail.com>
2021-08-06 22:37:22 +0700

    Tes auth commit

--- index.js
+++ index.js
@@ -10,9 +10,12 @@ const authRoute = require("./routes/auth");
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
-  console.log("Connected to DATABASE");
-});
+mongoose.connect(process.env.MONGO_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })   
+ .then(() => console.log("Database connected!"))
+ .catch(err => console.log(err));
 
 //middleware
 app.use(express.json());
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,15 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-    res.send("Hey its auth route")
-});
+router.get("/register", async (req, res) => {
+    const user = await new User({
+        username: "kalista",
+        email: "kalista@gmail.com",
+        password: "123456"
+    })
+
+    await user.save();
+    res.send("ok");
+})
 
 module.exports = router
